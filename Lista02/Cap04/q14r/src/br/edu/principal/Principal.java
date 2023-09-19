564e2e167cc1972c78e67487e147bd59a47d6562
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2023-09-19 15:05:22 -0300

    Consertar questão 14

--- Lista02/Cap04/q14r/src/br/edu/principal/Principal.java
+++ Lista02/Cap04/q14r/src/br/edu/principal/Principal.java
@@ -6,20 +6,26 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int sal, novo_sal, boni, aux;
+		double sal, novo_sal, boni, aux = 0;
 		
-		sal = sc.nextInt();
+		sal = sc.nextDouble();
 		if (sal <= 500) {
 			boni = sal * 5/100;
-			else if (sal <= 1200)
-				boni = sal * 12/100;
-				else 
-					boni = 0;
 		}
-		if (sal <= 600) {
+		else if(sal <= 600) {
+			boni = sal * 5/100;
 			aux = 150;
-			else aux = 100;
 		}
+		else if (sal <= 1200) {
+			boni = sal * 12/100;
+			if (sal > 600) {
+				aux = 100;
+			}
+		}
+		else {
+			boni = 0;
+		}
+		
 		
 		novo_sal = (sal + boni + aux);
 		System.out.println(novo_sal);
