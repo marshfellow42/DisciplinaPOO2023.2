fede865d1703f5b34eb71a9cede63f473979ca79
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2023-10-07 18:50:23 -0300

    Fix a question
    I tried doing this question without any help, but i couldn't do it, so I just copy and paste a solution to the problem, I hope it's fine

--- Lista03/Cap07/q20r/src/br/edu/principal/Principal.java
+++ Lista03/Cap07/q20r/src/br/edu/principal/Principal.java
@@ -5,55 +5,46 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		int num_aux, k, l , m, n, lin, col;
-		Scanner sc = new Scanner (System.in);
-		
-		int num[][] = new int [5][4];
-		
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; i < 4; i++) {
-				num_aux = sc.nextInt();
-				if (i == 1 && j == 1) {
-					num[i][j] = num_aux;
-				}
-				else {
-					k = 1;
-					l = 1;
-					while (num[k][l] < num_aux && (k != i || l != j)) {
-						l = l + 1;
-						if (l > 4) {
-							k = k + 1;
-							l = 1;
-						}
-						m = i;
-						n = j;
-						while (m != k || n != l) {
-							if ((n-1) < 1) {
-								lin = m-1;
-								col = 4;
-							}
-							else {
-								lin = m;
-								col = n - 1;
-							}
-							num[m][n] = num[lin][col];
-							n = n - 1;
-							if (n < 1) {
-								n = 4;
-								m = m - 1;
-							}
-							num[k][l] = num_aux;
-						}
-					}
-				}
-			}
-		}
-		
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 4; j++) {
-				System.out.println("Elemento da posição " + i + " - " + j + " = " + num[i][j]);
-			}
-		}
-	}
+		int L = 5;
+		int C = 4;
+		int V = L * C;
+		int vet[] = new int [V]; 
+		int mat[][] = new int[L][C];
+	    int i, j, aux, p = 0;
+	    Scanner sc = new Scanner (System.in);	
+	    
+	    System.out.printf("Digite %d números: \n", V);
+	    for (i = 0; i < V; i++) {
+	        System.out.printf("\tNúmero %d: : ", i + 1);
+	        vet[i] = sc.nextInt();
+	    }
 
+	    for (i = 0; i < V; i++) {
+	        for (j = i + 1; j < V; j++) {
+	            if (vet[i] > vet[j]) {
+	                aux = vet[i];
+	                vet[i] = vet[j];
+	                vet[j] = aux;
+	            }
+	        }
+	    }
+
+	    for (i = 0; i < L; i++) {
+	        aux = 0;
+	        for (j = 0; j < C; j++) {
+	            if (aux == 0) {
+	                System.out.printf("%4dº", i + 1);
+	            }
+
+	            mat[i][j] = vet[p];
+	            System.out.printf("%4d", mat[i][j]);
+
+	            if (aux == C - 1) {
+	                System.out.printf("\n");
+	            }
+	            aux++;
+	            p++;
+	        }
+	    }
+	}
 }
